f03057001a513ea89b90bdf7d3e8697db9a6062d
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-11-27 13:37:04 -0500

    add , put , delete method added

--- src/main/java/com/example/payroll_server/controller/EventController.java
+++ src/main/java/com/example/payroll_server/controller/EventController.java
@@ -2,9 +2,11 @@ package com.example.payroll_server.controller;
 
 import com.example.payroll_server.model.Event;
 import com.example.payroll_server.service.EventService;
-import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/events")
@@ -12,15 +14,23 @@ public class EventController {
     @Autowired
     EventService eventService;
 
-    @PutMapping("/{id}")
-    public Event updateEvent(@PathVariable Long id, @RequestBody Event eventDetails) {
-        Event event = eventService.findEventById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Event not found"));
-        event.setTitle(eventDetails.getTitle());
-        event.setStart(eventDetails.getStart());
-        event.setEnd(eventDetails.getEnd());
-        event.setColor(eventDetails.getColor());
-        return eventService.addEvent(event);
+    @GetMapping
+    public List<Event> getAllEvents() {
+        return eventService.findAllEvents();
     }
 
+    @GetMapping("/{id}")
+    public Optional<Event> getEventById(Long id) {
+        return eventService.findEventById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Event updateEvent(@PathVariable Long id, @RequestBody Event eventDetails) {
+        return eventService.updateEvent(id, eventDetails);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEventById(@PathVariable Long id) {
+        eventService.deleteEventById(id);
+    }
 }
--- src/main/java/com/example/payroll_server/service/EventService.java
+++ src/main/java/com/example/payroll_server/service/EventService.java
@@ -2,8 +2,11 @@ package com.example.payroll_server.service;
 
 import com.example.payroll_server.model.Event;
 import com.example.payroll_server.repositories.EventRepository;
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,7 +19,15 @@ public class EventService {
     public Event addEvent(Event event) {
         return eventRepository.save(event);
     }
-
+    public Event updateEvent(Long id, Event eventDetails) {
+        Event event = eventRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Event not found"));
+        event.setTitle(eventDetails.getTitle());
+        event.setComment(eventDetails.getComment());
+        event.setEnd(eventDetails.getEnd());
+        event.setStart(eventDetails.getStart());
+        event.setColor(eventDetails.getColor());
+        return eventRepository.save(event);
+    }
     public List<Event> findAllEvents() {
         return eventRepository.findAll();
     }
